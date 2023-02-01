2e654530fe1c811a5d78c2ad9cb77af9da8010f7
Aleksander Chumachenko <ledich19@gmail.com>
2023-02-01 13:20:27 +0200

    feat: add FieldCard component

---
+++ src/components/GameField/FieldCard/FieldCard.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import './FieldCard.scss';
+import { BoardItemType } from './../../../app/types';
+
+type PropsType = {
+  heightField: number;
+  item: BoardItemType;
+};
+
+const FieldCard = ({ heightField, item }: PropsType) => {
+  const style = {
+    height: `calc(100vh / ${heightField})`,
+    width: `calc(100vh / ${heightField})`,
+    borderLeft: !item.left ? 'solid 4px black' : '',
+    borderRight: !item.right ? 'solid 4px black' : '',
+    borderTop: !item.top ? 'solid 4px black' : '',
+    borderBottom: !item.bottom ? 'solid 4px black' : '',
+  };
+
+  return (
+    <div style={style} className="field-card">
+      {item.id}
+    </div>
+  );
+};
+
+export default FieldCard;
--- src/components/GameField/GameField.tsx
+++ src/components/GameField/GameField.tsx
@@ -1,35 +1,18 @@
 import React from 'react';
 import { useAppSelector } from '../../app/hooks';
+import FieldCard from './FieldCard/FieldCard';
 import './GameField.scss';
 
 const GameField = () => {
   const gameFieldMatrix = useAppSelector((state) => state.game);
   const heightField = gameFieldMatrix.length;
-  const widthField = gameFieldMatrix[0].length;
-  // const itemSize = {
-  //   height: `calc(100vh / ${heightField})`,
-  //   width: `calc(100vh / ${heightField})`,
-  // };
 
   return (
     <div className="field">
       {gameFieldMatrix.map((row, i) => {
         return (
           <div className="field__row" key={`rowId${i}`}>
-            {row.map((item) => {
-              const style = {
-                height: `calc(100vh / ${heightField})`,
-                width: `calc(100vh / ${heightField})`,
-                borderLeft: !item.left ? 'solid 4px black' : '',
-                borderRight: !item.right ? 'solid 4px black' : '',
-                borderTop: !item.top ? 'solid 4px black' : '',
-                borderBottom: !item.bottom ? 'solid 4px black' : '',
-              };
-              return <div style={style} key={item.id} className="field__item">
-               {/* <img src={`./images/characters/boris.png`} alt="" className='field__img'/> */}
-                
-                {item.id}</div>;
-            })}
+            {row.map((item) => <FieldCard key={item.id} heightField={heightField} item={item}/>)}
           </div>
         );
       })}
