17e99addcd80e41b699f84ed063531abfb27a4cf
Aleksander Chumachenko <ledich19@gmail.com>
2023-02-02 19:33:41 +0200

    style: fix eslint problems

--- src/Modules/Header/Header.tsx
+++ src/Modules/Header/Header.tsx
@@ -1,30 +1,23 @@
-import React from "react";
-import { Link } from "react-router-dom";
-import "./Header.scss";
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './Header.scss';
 
-const Header = () => {
-  return (
-    <nav>
-      <ul className="header__list">
-        <Link rel="stylesheet" to={`/`} className="header__link">
-          Home
-        </Link>
-        <Link rel="stylesheet" to={`/game`} className="header__link">
-          game
-        </Link>
-        <Link rel="stylesheet" to={`/tutorial`} className="header__link">
+const Header = () => (
+  <nav>
+    <ul className="header__list">
+      <Link rel="stylesheet" to={'/'} className="header__link">
+        Home
+      </Link>
+      <Link rel="stylesheet" to={'/game'} className="header__link">
+        game
+      </Link>
+      <Link rel="stylesheet" to={'/tutorial'} className="header__link">
         guide
-        </Link>
-        <Link rel="stylesheet" to={`/404`} className="header__link">
-          404
-        </Link>
-      </ul>
-    </nav>
-  );
-};
-
+      </Link>
+      <Link rel="stylesheet" to={'/404'} className="header__link">
+        404
+      </Link>
+    </ul>
+  </nav>
+);
 export default Header;
-
-
-
-
