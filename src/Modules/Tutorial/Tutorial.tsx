4b0913cdee59af37387ffdf857a21ccedee3650e
pedanmax <m.pedan2017@gmail.com>
2023-02-03 17:52:46 +0200

    feat:add inBox item

--- src/Modules/Tutorial/Tutorial.tsx
+++ src/Modules/Tutorial/Tutorial.tsx
@@ -1,12 +1,12 @@
 import React from 'react'
-import { useAppSelector, useAppDispatch } from '../../app/hooks';
-import Item from './item-component/item';
+import { useAppSelector } from '../../app/hooks';
+import Item from './goal-component/item';
+import InBox from './inBox-component/inBox';
 import "./Tutorial.scss";
 
 
 const Tutorial = () => {
   const rules = useAppSelector(state => state.rules);
-  
   return (
     <div className="rules">
       <div className="rules__container">
@@ -14,16 +14,15 @@ const Tutorial = () => {
           <img src="images/tutorial_page/logo.png" alt="logo" className="rules__logo" />
           <div className='rules__info'>
             <div className="rules__table table">
-              <div className='table__wrapper'>
-                <ul className="table__list">
-                  {rules.table.text.map((rule, index) => <li key={index} className="table__item">{rule}</li>)}
-                </ul>
-              </div >
+              <ul className="table__list">
+                {rules.table.text.map((rule, index) => <li key={index} className="table__item">{rule}</li>)}
+              </ul>
             </div>
             <p className="rules__about-game">{rules.aboutGame}</p>
           </div>
         </div>
-        <Item/>
+        <Item />
+        <InBox />
       </div>
       
     </div>
---
+++ src/Modules/Tutorial/inBox-component/inBox.tsx
@@ -0,0 +1,46 @@
+import React, {useState} from "react";
+import { useAppSelector } from "../../../app/hooks";
+import './inBox.scss';
+
+const InBox = () => {
+  const rules = useAppSelector(state => state.rules);
+  const {inBox} = rules;
+
+  const [visible, setVisibility] = useState('inbox__content visible');
+  const [activity, setActivity] = useState('rotate(0deg)');
+
+  const changeVisibility = () => {
+    if(visible === 'inbox__content visible'){
+      setVisibility('inbox__content');
+      setActivity('rotate(180deg)');
+    }
+    else {
+      setVisibility('inbox__content visible');
+      setActivity('rotate(0deg)');
+    }
+  }
+  return (
+    <div className="inbox item">
+      <div className="item__top">
+        <h4 className="item__title">{inBox.title}</h4>
+        <button style={{transform: activity}} className="item__btn" onClick={changeVisibility}></button>
+      </div >
+      <div className={visible}>
+        {Object.values(inBox.cards).map((category,index) =>{
+            return (
+              <div key={index} className="inbox__element element">
+                <h5 className="element__title">{category.title}</h5>
+                <ul className="element__list">
+                  {Object.values(category).slice(1).map((item,index)=>{
+                    return (
+                    <li key={index} className="element__item-list">{item}</li>
+                    )})}
+                </ul>
+              </div>
+            )
+          })}
+      </div>
+    </div>
+  )
+};
+export default InBox;
